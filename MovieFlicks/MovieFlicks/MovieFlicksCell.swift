b7c69721d11d533f5cdab65890fb1c6b52f48678
lusop <lusop@walmartlabs.com>
2016-07-18 23:09:29 -0700

    initial commit

---
+++ MovieFlicks/MovieFlicks/MovieFlicksCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieFlicksCell.swift
+//  MovieFlicks
+//
+//  Created by Ledesma Usop Jr. on 7/18/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieFlicksCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
